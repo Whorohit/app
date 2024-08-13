03a26eba9fc16f2be0f1b49df347b2b66752d87b
Whorohit <ram21296@gmail.com>
2024-08-13 15:59:42 +0530

    hello

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+// const cors = require('cors');
+// const path = require('path'); 
+// app.use(cors()); // Allow CORS for all routes by default
+// // Route for the home page
+// app.get('/', (req, res) => {
+//   res.send('Hello, World!');
+// });
+
+// // Start the server
+// app.use(express.static('public'));
+
+// // Route to send the JavaScript file
+// // app.get('/adPopup.js', (req, res) => {
+//     // res.sendFile(path.join(__dirname, 'adPopup.js'));
+// // });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+
 const express = require('express');
 const app = express();
 const port = 3000;
 const cors = require('cors');
-const path = require('path'); 
-app.use(cors()); // Allow CORS for all routes by default
+
+// Allow CORS for all routes by default
+app.use(cors()); 
+
+// Serve static files from the 'public' directory
+app.use(express.static('public')); 
+
 // Route for the home page
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
 // Start the server
-app.use(express.static('public_html'));
-
-// Route to send the JavaScript file
-// app.get('/adPopup.js', (req, res) => {
-    // res.sendFile(path.join(__dirname, 'adPopup.js'));
-// });
-
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
