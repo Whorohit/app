ae28fefe9213a17525352021605630a7e870c8f0
Whorohit <ram21296@gmail.com>
2024-08-13 16:53:57 +0530

    commit

--- index.js
+++ index.js
@@ -1,66 +1,78 @@
+// // // const express = require('express');
+// // // const app = express();
+// // // const port = 3000;
+// // // const cors = require('cors');
+// // // const path = require('path'); 
+// // // app.use(cors()); // Allow CORS for all routes by default
+// // // // Route for the home page
+// // // app.get('/', (req, res) => {
+// // //   res.send('Hello, World!');
+// // // });
+
+// // // // Start the server
+// // // app.use(express.static('public'));
+
+// // // // Route to send the JavaScript file
+// // // // app.get('/adPopup.js', (req, res) => {
+// // //     // res.sendFile(path.join(__dirname, 'adPopup.js'));
+// // // // });
+
+// // // app.listen(port, () => {
+// // //   console.log(`Server is running on http://localhos/t:${port}`);
+// // // });
+
 // // const express = require('express');
 // // const app = express();
 // // const port = 3000;
 // // const cors = require('cors');
-// // const path = require('path'); 
-// // app.use(cors()); // Allow CORS for all routes by default
+
+// // // Allow CORS for all routes by default
+// // app.use(cors({
+// //   origin: '*', // Allow all origins (for development purposes)
+// // }));
+
+// // // Serve static files from the 'public' directory
+// // app.use(express.static('public')); 
+
 // // // Route for the home page
 // // app.get('/', (req, res) => {
 // //   res.send('Hello, World!');
 // // });
 
 // // // Start the server
-// // app.use(express.static('public'));
-
-// // // Route to send the JavaScript file
-// // // app.get('/adPopup.js', (req, res) => {
-// //     // res.sendFile(path.join(__dirname, 'adPopup.js'));
-// // // });
-
 // // app.listen(port, () => {
-// //   console.log(`Server is running on http://localhos/t:${port}`);
+// //   console.log(`Server is running on http://localhost:${port}`);
 // // });
 
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-// const cors = require('cors');
 
-// // Allow CORS for all routes by default
-// app.use(cors({
-//   origin: '*', // Allow all origins (for development purposes)
-// }));
+// const express = require('express') 
+// const path = require('path') 
+// const app = express() 
 
-// // Serve static files from the 'public' directory
-// app.use(express.static('public')); 
+// // Static Middleware 
+// app.use(express.static(path.join(__dirname, 'public'))) 
 
-// // Route for the home page
-// app.get('/', (req, res) => {
-//   res.send('Hello, World!');
-// });
+// // View Engine Setup 
+// app.set('views', path.join(__dirname, 'views')) 
+// app.set('view engine', 'ejs') 
 
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
+// app.get('/', function(req, res){ 
+// 	res.render('Demo') 
+// }) 
 
+// app.listen(8080, function(error){ 
+// 	if(error) throw error 
+// 	console.log("Server created Successfully") 
+// }) 
 
-const express = require('express') 
-const path = require('path') 
-const app = express() 
+const express = require('express');
+const path = require('path');
+const app = express();
 
-// Static Middleware 
-app.use(express.static(path.join(__dirname, 'public'))) 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-// View Engine Setup 
-app.set('views', path.join(__dirname, 'views')) 
-app.set('view engine', 'ejs') 
-
-app.get('/', function(req, res){ 
-	res.render('Demo') 
-}) 
-
-app.listen(8080, function(error){ 
-	if(error) throw error 
-	console.log("Server created Successfully") 
-}) 
+// Start the server
+app.listen(8080, () => {
+    console.log("Server running on http://localhost:8080");
+});
